abaf9d869724174fb0ed94203902e89ad6585f0e
Pavel Svec <pavel.svec@itcrawl.cz>
2021-01-14 15:08:59 +0100

    Model - View - ViewModel ve WPF

---
+++ ModelViewViewModel/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ModelViewViewModel.Views;
+using ModelViewViewModel.Models;
+
+namespace ModelViewViewModel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Ve WPF se nepoužívá Open, ale přiřazuje se např. obsah do Content
+        // Dokud se tu neřeší obchodní logika (práce s daty, pravidly, atd.), je to OK
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page1 p = new Page1();
+            this.Content = p;
+        }
+
+        // Prověření, že ve finále se změnila data v modelu...
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show(ZpravaModel.TextZpravy);
+        }
+    }
+}
---
+++ ModelViewViewModel/ViewModels/ZpravaViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+// Tohle je třeba nainstalovat přes NuGet, jinak Command dá moc práce
+using GalaSoft.MvvmLight.Command;
+using ModelViewViewModel.Models;
+
+namespace ModelViewViewModel.ViewModels
+{
+
+
+    class ZpravaViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string zprava;
+        public string Zprava
+        {
+            get { return zprava; }
+            set 
+            { 
+                zprava = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Zprava"));
+            }
+        }
+
+        public void ShowDialogZprava()
+        {
+            MessageBox.Show(Zprava);
+        }
+
+        public ZpravaViewModel()
+        {
+            Zprava = ZpravaModel.TextZpravy;
+        }
+
+        private static ICommand _sendCommand;
+
+        // SendCommand je bindovaný z GUI
+        public ICommand SendCommand
+        {
+            get
+            {
+                if (_sendCommand == null)
+                {
+                    // RelayCommand je definovaný v MVVMLight
+                    _sendCommand = new RelayCommand(
+                        () => { 
+                            // Tady je práce, která se má odmakat, když se spustí command
+                            ShowDialogZprava();
+                            ZpravaModel.TextZpravy = Zprava;
+                        });
+                }
+                return _sendCommand;
+            }
+        }
+
+    }
+}
